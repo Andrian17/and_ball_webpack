39b07a5ab3d3315c491f37e5879026d0ecb74349
Andrian17 <andriancimen123@gmail.com>
2022-12-31 20:23:26 +0800

    add assets botstrap to local

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,6 +2,9 @@ import 'regenerator-runtime';
 import '../public/style/style.css';
 import swRegister from './utils/sw-register.js';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
 import main from './view/main';
 
 // document.addEventListener('DOMContentLoaded', main);
